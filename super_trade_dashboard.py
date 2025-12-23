7acb8667e08224957678f630af152efbac672247
tjgreen649-hue <tjgreen649@gmail.com>
2025-12-23 07:18:09 -0500

    Update super_trade_dashboard.py

--- super_trade_dashboard.py
+++ super_trade_dashboard.py
@@ -74,7 +74,7 @@ starting_balance = st.sidebar.number_input(
     value=25_000,
     step=1000
 )
-for tf, enabled in timeframes.items():
+for tf, enabled in TimeFrames.items():
     if not enabled:
                    continue
 
