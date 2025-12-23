713f6df7d1f72d6a569be3e5307ab9c18f0803af
tjgreen649-hue <tjgreen649@gmail.com>
2025-12-23 05:36:57 -0500

    Update super_trade_dashboard.py

--- super_trade_dashboard.py
+++ super_trade_dashboard.py
@@ -121,7 +121,7 @@ timeframes = {
  show_volume = st.checkbox("Show Volume", value=True)
  sma_20 = close_series.rolling(20).mean()
  sma_50 = close_series.rolling(50).mean()
-)
+) 
  if sma_20.iloc[-1] > sma_50.iloc[-1]:
         return 1   # Bullish bias
     elif sma_20.iloc[-1] < sma_50.iloc[-1]:
