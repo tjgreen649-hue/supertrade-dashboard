07cf7d9e89f7af67e56e306a47681ed28717d006
tjgreen649-hue <tjgreen649@gmail.com>
2025-12-25 19:21:27 -0500

    Update super_trade_dashboard.py

--- super_trade_dashboard.py
+++ super_trade_dashboard.py
@@ -291,7 +291,7 @@ df["Factor_Score"] = (
 # =========================
 # TRADE TYPE CLASSIFICATION
 # =========================
-df["High_Confidence"] = df["Profit_Confidence"] 
+df["Profit_Confidence"] = df["Profit_Confidence"] 
 
 df["SMA_signal"] * weights["SMA_signal"] ( +
     df["EMA_signal"] * weights["EMA_signal"] +
